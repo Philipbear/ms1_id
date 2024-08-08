cfc02363da41c0ba07fcde96ef99da35d45394a9
Philipbear <philipxsp@hotmail.com>
2024-08-08 10:40:25 -0700

    group mz, chunk mz

--- bin/imaging/_group_mz_cor_parallel.py
+++ bin/imaging/_group_mz_cor_parallel.py
@@ -9,11 +9,10 @@ from _utils_imaging import PseudoMS1
 
 def generate_pseudo_ms1(mz_values, intensity_matrix, correlation_matrix,
                         n_processes=None, min_cluster_size=6,
-                        save=False, save_dir=None):
+                        save=False, save_dir=None, chunk_size=1000):
     """
-    Generate pseudo MS1 spectra for imaging data
+    Generate pseudo MS1 spectra for imaging data using chunked parallel processing
     """
-
     # Check if result files exist
     if save_dir:
         save_path = os.path.join(save_dir, 'pseudo_ms1_spectra.pkl')
@@ -24,7 +23,8 @@ def generate_pseudo_ms1(mz_values, intensity_matrix, correlation_matrix,
     # Perform clustering
     pseudo_ms1_spectra = _perform_clustering(mz_values, correlation_matrix,
                                              n_processes=n_processes,
-                                             min_cluster_size=min_cluster_size)
+                                             min_cluster_size=min_cluster_size,
+                                             chunk_size=chunk_size)
 
     # Assign intensity values
     _assign_intensities(pseudo_ms1_spectra, intensity_matrix)
@@ -37,53 +37,50 @@ def generate_pseudo_ms1(mz_values, intensity_matrix, correlation_matrix,
     return pseudo_ms1_spectra
 
 
-def _process_mz(args):
+def _process_chunk(args):
     """
-    Process a single m/z value for clustering.
+    Process a chunk of m/z values for clustering.
     """
-    i, mz, mz_values, correlation_matrix, min_cluster_size = args
+    start_idx, end_idx, mz_values, correlation_matrix, min_cluster_size = args
+    chunk_results = []
 
-    # Get the row of the correlation matrix for the current m/z
-    row = correlation_matrix[i].toarray().flatten()
+    for i in range(start_idx, end_idx):
+        mz = mz_values[i]
+        row = correlation_matrix[i].toarray().flatten()
+        cluster_indices = np.nonzero(row)[0]
 
-    # Find all m/z values with non-zero correlation scores
-    cluster_indices = np.nonzero(row)[0]
+        if len(cluster_indices) >= min_cluster_size:
+            cluster_mzs = mz_values[cluster_indices]
+            indices = cluster_indices.tolist()
+            chunk_results.append(PseudoMS1(cluster_mzs.tolist(), [0] * len(cluster_mzs), indices))
 
-    if len(cluster_indices) >= min_cluster_size:
-        # Form a pseudo MS1 spectrum
-        cluster_mzs = mz_values[cluster_indices]
-
-        # Store the actual indices in the original mz_values array
-        indices = cluster_indices.tolist()
-
-        return PseudoMS1(cluster_mzs.tolist(), [0] * len(cluster_mzs), indices)
-
-    return None
+    return chunk_results
 
 
 def _perform_clustering(mz_values, correlation_matrix, n_processes=None,
-                        min_cluster_size=6):
+                        min_cluster_size=6, chunk_size=1000):
     """
-    Perform clustering on m/z values based on correlation scores using multiprocessing.
+    Perform clustering on m/z values based on correlation scores using chunked multiprocessing.
     """
-    # Ensure correlation_matrix is in CSR format for efficient row slicing
     if not isinstance(correlation_matrix, csr_matrix):
         correlation_matrix = csr_matrix(correlation_matrix)
 
-    # Prepare arguments for multiprocessing
-    args_list = [
-        (i, mz, mz_values, correlation_matrix, min_cluster_size)
-        for i, mz in enumerate(mz_values)]
-
-    # Use multiprocessing to process m/z values in parallel
     if n_processes is None:
         n_processes = mp.cpu_count()
 
-    with mp.Pool(processes=n_processes) as pool:
-        results = list(tqdm(pool.imap(_process_mz, args_list), total=len(args_list), desc="Clustering"))
+    # Prepare chunks
+    n_chunks = (len(mz_values) + chunk_size - 1) // chunk_size
+    chunks = [(i * chunk_size, min((i + 1) * chunk_size, len(mz_values)),
+               mz_values, correlation_matrix, min_cluster_size)
+              for i in range(n_chunks)]
 
-    # Filter out None results and create the pseudo_ms1_spectra list
-    pseudo_ms1_spectra = [result for result in results if result is not None]
+    # Process chunks in parallel
+    with mp.Pool(processes=n_processes) as pool:
+        results = list(tqdm(pool.imap(_process_chunk, chunks),
+                            total=len(chunks), desc="Processing chunks"))
+
+    # Flatten results
+    pseudo_ms1_spectra = [spectrum for chunk_result in results for spectrum in chunk_result]
 
     # Remove redundant spectra
     non_redundant_spectra = _remove_redundant_spectra(pseudo_ms1_spectra)
@@ -96,14 +93,9 @@ def _assign_intensities(pseudo_ms1_spectra, intensity_matrix):
     Assign intensity values to pseudo MS1 spectra.
     """
     for spectrum in tqdm(pseudo_ms1_spectra, desc="Assigning intensities"):
-        # Get intensities for these indices
         intensities = intensity_matrix[spectrum.indices, :]
-
-        # Find the spectrum with the highest total intensity for this cluster
         spectrum_sums = np.sum(intensities, axis=0)
         max_spectrum_index = np.argmax(spectrum_sums)
-
-        # Select the intensities from the spectrum with the highest total intensity
         spectrum.intensities = intensities[:, max_spectrum_index].tolist()
 
 
